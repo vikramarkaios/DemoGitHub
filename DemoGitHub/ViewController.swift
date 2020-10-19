df84583781b07d84c33073b43237d359705d2f83
vikramarka <vikramarkaios@gmail.com>
2020-10-19 15:15:49 +0530

    committing folders

---
+++ DemoGitHub/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  DemoGitHub
+//
+//  Created by vikramarka reddy on 19/10/20.
+//  Copyright © 2020 vikramarka reddy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        print("Hello vikramarka, you done great job. Keep it up and go well futher life.")
+    }
+
+
+}
+
