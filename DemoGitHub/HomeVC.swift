0581c5b00f6084d93c8a56f449002953e6c82c6f
vikramarka <vikramarkaios@gmail.com>
2020-10-19 17:15:17 +0530

    Added home vc and storyboard

---
+++ DemoGitHub/HomeVC.swift
@@ -0,0 +1,30 @@
+//
+//  HomeVC.swift
+//  DemoGitHub
+//
+//  Created by vikramarka reddy on 19/10/20.
+//  Copyright © 2020 vikramarka reddy. All rights reserved.
+//
+
+import UIKit
+
+class HomeVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
